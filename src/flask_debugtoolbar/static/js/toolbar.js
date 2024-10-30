2439e45809fa062e675b6ddab49426078bd2a389
Peter Darrow <pmdarrow@gmail.com>
2024-10-30 13:57:11 -0300

    Remove use of cookies as they're not supported in Insomnia HTML previews

--- src/flask_debugtoolbar/static/js/toolbar.js
+++ src/flask_debugtoolbar/static/js/toolbar.js
@@ -1,8 +1,5 @@
 (function($) {
-  $.cookie = function(name, value, options) { if (typeof value != 'undefined') { options = options || {}; if (value === null) { value = ''; options.expires = -1; } var expires = ''; if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) { var date; if (typeof options.expires == 'number') { date = new Date(); date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000)); } else { date = options.expires; } expires = '; expires=' + date.toUTCString(); } var path = options.path ? '; path=' + (options.path) : ''; var domain = options.domain ? '; domain=' + (options.domain) : ''; var secure = options.secure ? '; secure' : ''; document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join(''); } else { var cookieValue = null; if (document.cookie && document.cookie != '') { var cookies = document.cookie.split(';'); for (var i = 0; i < cookies.length; i++) { var cookie = $.trim(cookies[i]); if (cookie.substring(0, name.length + 1) == (name + '=')) { cookieValue = decodeURIComponent(cookie.substring(name.length + 1)); break; } } } return cookieValue; } };
   $('head').append('<link rel="stylesheet" href="'+DEBUG_TOOLBAR_STATIC_PATH+'css/toolbar.css?'+ Math.random() +'" type="text/css" />');
-  var COOKIE_NAME = 'fldt';
-  var COOKIE_NAME_ACTIVE = COOKIE_NAME +'_active';
   var fldt = {
     init: function() {
       $('#flDebug').show();
@@ -24,32 +21,14 @@
         return false;
       });
       $('#flDebugPanelList li .flDebugSwitch').on('click', function() {
-        var $panel = $(this).parent();
         var $this = $(this);
-        var dom_id = $panel.attr('id');
-
-        // Turn cookie content into an array of active panels
-        var active_str = $.cookie(COOKIE_NAME_ACTIVE);
-        var active = (active_str) ? active_str.split(';') : [];
-        active = $.grep(active, function(n,i) { return n != dom_id; });
 
         if ($this.hasClass('flDebugActive')) {
           $this.removeClass('flDebugActive');
           $this.addClass('flDebugInactive');
         } else {
-          active.push(dom_id);
           $this.removeClass('flDebugInactive');
           $this.addClass('flDebugActive');
-        }
-
-        if (active.length > 0) {
-          $.cookie(COOKIE_NAME_ACTIVE, active.join(';'), {
-            path: '/', expires: 10
-          });
-        } else {
-          $.cookie(COOKIE_NAME_ACTIVE, null, {
-            path: '/', expires: -1
-          });
         }
       });
       $('#flDebug a.flDebugClose').on('click', function() {
@@ -77,7 +56,7 @@
         return false;
       });
       $('#flDebugHideToolBarButton').on('click', function() {
-        fldt.hide_toolbar(true);
+        fldt.hide_toolbar();
         return false;
       });
       $('#flDebugShowToolBarButton').on('click', function() {
@@ -97,15 +76,11 @@
         }
         // Otherwise, just minimize the toolbar
         if ($('#flDebugToolbar').is(':visible')) {
-          fldt.hide_toolbar(true);
+          fldt.hide_toolbar();
           return;
         }
       });
-      if ($.cookie(COOKIE_NAME)) {
-        fldt.hide_toolbar(false);
-      } else {
-        fldt.show_toolbar(false);
-      }
+      fldt.show_toolbar(false);
       $('#flDebug table.flDebugTablesorter').each(function() {
           var headers = {};
           $(this).find('thead th').each(function(idx, elem) {
@@ -133,7 +108,7 @@
       $(document).trigger('close.flDebug');
       return false;
     },
-    hide_toolbar: function(setCookie) {
+    hide_toolbar: function() {
       // close any sub panels
       $('#flDebugWindow').hide();
       // close all panels
@@ -144,12 +119,6 @@
       $('#flDebugToolbarHandle').show();
       // Unbind keydown
       $(document).unbind('keydown.flDebug');
-      if (setCookie) {
-        $.cookie(COOKIE_NAME, 'hide', {
-          path: '/',
-          expires: 10
-        });
-      }
     },
     show_toolbar: function(animate) {
       // Set up keybindings
@@ -164,10 +133,6 @@
       } else {
         $('#flDebugToolbar').show();
       }
-      $.cookie(COOKIE_NAME, null, {
-        path: '/',
-        expires: -1
-      });
     },
     toggle_arrow: function(elem) {
       var uarr = String.fromCharCode(0x25b6);
